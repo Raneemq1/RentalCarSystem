271a630f013ac1210b8e05bd7afb4908af264913
Raneemq1 <raneemalqadi32@gmail.com>
2024-07-14 13:13:46 +0300

    solve task

---
+++ RentalCarSystem/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Shared;
+
+namespace RentalCarSystem.Controllers
+{
+    public class BaseController:Controller
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if(HttpContext.Session.GetString("User") is null)
+            {
+                context.Result = RedirectToPage("/Account/Login", new { area = "Identity" });
+                return;
+            }
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ RentalCarSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RentalCarSystem.Models;
+
+namespace RentalCarSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<User>().ToTable("users");
+            
+        }
+    }
+}
